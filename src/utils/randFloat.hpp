a3d01e7d662f70e87bcf7a2f851890c42594ed3c
Nils Moreau--Thomas <terminatorletrol@gmail.com>
2026-05-26 21:28:34 +0200

    added random lib

--- src/utils/randFloat.hpp
+++ src/utils/randFloat.hpp
@@ -1,7 +1,4 @@
-float randF() {
-    return (float)rand() / (float)RAND_MAX;
-}
+#pragma once
 
-float randF(float a, float b) {
-    return a + (b - a) * ((float)rand() / (float)RAND_MAX);
-}
+float randF();
+float randF(float a, float b);
